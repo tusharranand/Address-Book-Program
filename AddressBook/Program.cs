7d337f3392fd40c828b413977d0ebfcb7ae8e8f0
Tushar Anand <tusharranand@gmail.com>
2022-03-16 20:50:33 +0530

    [Tushar Anand] Added: Adding dictionary to save added contact

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -5,21 +5,19 @@ namespace AddressBook
 {
     class Contacts
     {
-        public string First_Name = "";
-        public string Last_Name = "";
+        public static string First_Name = "";
+        public static string Last_Name = "";
         public string Full_Name = "";
-        public string Address = "";
-        public string City = "";
-        public string State = "";
-        public string Zip_Code = "";
-        public string Phone_Number = "";
-        public string Email = "";
-        public Contacts()
-        {
-            Console.Write("Enter First Name: ");
-            First_Name = Console.ReadLine();
-            Console.Write("Enter Last Name: ");
-            Last_Name = Console.ReadLine();
+        public static string Address = "";
+        public static string City = "";
+        public static string State = "";
+        public static string Zip_Code = "";
+        public static string Phone_Number = "";
+        public static string Email = "";
+        public Contacts(string First, string Last)
+        { 
+            First_Name = First;
+            Last_Name = Last;  
             Full_Name = First_Name.ToLower() + Last_Name.ToLower();
             Console.Write("Enter Address: ");
             Address = Console.ReadLine();
@@ -33,14 +31,28 @@ namespace AddressBook
             Phone_Number = Console.ReadLine();
             Console.Write("Enter Email Address: ");
             Email = Console.ReadLine();
+            
         }
     }
     class AddressBookMain
     {
+        public Dictionary<string, Contacts> Book = new Dictionary<string, Contacts>();
+        public void AddAddress()
+        {
+            string First = "";
+            string Last = "";
+            Console.Write("Enter First Name: ");
+            First = Console.ReadLine();
+            Console.Write("Enter Last Name: ");
+            Last = Console.ReadLine();
+            Contacts Details = new Contacts(First, Last);
+            Book.Add(Details.Full_Name, Details);
+        }
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book Program");
-            Contacts Details = new Contacts();
+            AddressBookMain Record = new AddressBookMain();
+            Record.AddAddress();  
         }
     }
 }
